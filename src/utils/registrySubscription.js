6515bedcb3035bae67d4f91974088d566fe5e1fc
Asarck <85520089+Asarck@users.noreply.github.com>
2024-04-10 08:56:08 +0200

    route protection

--- src/utils/registrySubscription.js
+++ src/utils/registrySubscription.js
@@ -18,18 +18,7 @@ module.exports = {
                     redirectUrl: "/api/orders",
                     routeProtections: [
                         // Orders
-                        { methods: ["GET"], route: "/", roles: ["restaurantOwner", "user", "deliveryman", "admin", "salesman"] },
-                        { methods: ["POST"], route: "/", roles: ["user", "admin"] },
-                        { methods: ["POST"], route: "/:id/restaurant-checked", roles: ["restaurantOwner", "admin"] },
-                        { methods: ["POST"], route: "/:id/deliveryman-checked", roles: ["deliveryman", "admin"] },
-                        { methods: ["POST"], route: "/:id/prepared", roles: ["restaurantOwner", "admin"] },
-                        { methods: ["POST"], route: "/:id/delivered", roles: ["deliveryman", "admin"] },
-                        { methods: ["POST"], route: "/:id/abort", roles: ["user", "restaurantOwner", "admin"] },
-                        { methods: ["GET"], route: "/:id", roles: ["restaurantOwner", "user", "deliveryman", "admin", "salesman"] },
-                        { methods: ["PATCH", "PUT", "DELETE"], route: "/:id", roles: ["user", "admin"] },
-                        // Status
-                        { methods: ["POST"], route: "/status", roles: ["admin", "technician"] },
-                        { methods: ["DELETE", "PATCH"], route: "/status/:id", roles: ["admin", "technician"] }
+                        { methods: ["GET"], route: "/", roles: ["admin", "technician"] },
                     ]
                 }
             });
